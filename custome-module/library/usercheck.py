29c40e1f2eb7459a911b3a95943d080e23c41c1b
root <root@ip-172-31-47-48.us-east-2.compute.internal>
2019-05-04 13:42:05 +0000

    My First Commit

---
+++ custome-module/library/usercheck.py
@@ -0,0 +1,29 @@
+from ansible.module_utils.basic import *
+
+
+def is_user_exist(username) :
+  try:
+    import pwd
+    return [entry.pw_shell for entry in pwd.getpwall() if entry.pw_name="root"]
+  except:
+    module.fail_json(msg="Module pwd not found")
+
+
+
+def main():
+  module = AnsibleModule(argument_spec = dict(username = dict(required=True)))
+  username = module.params.get("username")
+
+  exists = is_user_exist(username)
+  if exists:
+    msg = "{0} is found".format(username)
+  else:
+    msg = "{0} not found".format(username)
+  
+
+
+  module.exit_json(changed=True, msg=msg)
+
+
+
+main()
